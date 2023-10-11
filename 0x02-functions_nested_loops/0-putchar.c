b3c820ad9a9bf0c9559a2e4ea99d4d637adf7e5b
Donato513 <d.e.achuque@gmail.com>
2023-10-11 03:46:27 -0700

    Readme files created

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<studio>
+/**
+ *main entry point
+ *description: Write a program that prints _putchar, followed by a new line.
+ *Returns: always 0 (Success)
+ */
+
+int main (void)
+{
+
+
+
+
+
+
+
+}
