3e05b46f1f828adfbcc98e732f19af59f83b4d27
Donato513 <d.e.achuque@gmail.com>
2023-10-26 09:34:30 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
 /**
+ * _print_rev_recursion - Print a string in reverse recursively.
+ *
+ * @s: The string to reverse and print.
+ */
+void _print_rev_recursion(char *s);
+
+/**
  * _strlen_recursion - Calculate the length of a string recursively.
  *
  * @s: The string to calculate the length of.
@@ -13,15 +20,20 @@ int _strlen_recursion(char *s)
 	{
 	return (0);
 	}
-	else
-	{
+
 	s++;
 	_print_rev_recursion(s);
 	s--;
 	_putchar(*s);
 	return (1 + _strlen_recursion(s));
-	}
 }
+
+/* Definition of the _print_rev_recursion function */
+/**
+ * _print_rev_recursion - Print a string in reverse recursively.
+ *
+ * @s: The string to reverse and print.
+ */
 
 void _print_rev_recursion(char *s)
 {
@@ -32,3 +44,4 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
+
