46d896cf8f3779ab027e5883fac5a121aa0a012c
Donato513 <d.e.achuque@gmail.com>
2023-11-02 10:14:29 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,49 +2,75 @@
 #include <stdlib.h>
 
 /**
- * _isdigit - Check if a string consists of digits.
- * @argv: Pointer to the string.
- * Return: 0 if all digits, 1 if not all digits.
- */
-int _isdigit(char *argv)
-{
-	int i = 0;
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
+  */
 
+int _isdigit(char *argv)
+
+{
+	int i;
+
+	i = 0;
 	while (argv[i])
 	{
-	if (argv[i] < '0' || argv[i] > '9')
-	return (1);
-	i++;
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
 	}
-
 	return (0);
 }
 
 /**
- * main - Main function with arguments.
- * @argc: Argument count.
- * @argv: Argument array.
- * Return: 0 on success, 98 on failure.
- */
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+
+int _atoi(char *s)
+
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+
+/**
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+  */
+
 int main(int argc, char *argv[])
+
 {
 	int i;
 
 	if (argc != 3)
 	{
-	printf("Error\n");
-	return (98);
+		printf("Error\n");
+		exit(98);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
-	if (_isdigit(argv[i]))
-	{
-	printf("Error\n");
-	return (98);
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 	}
-	}
-
 	return (0);
 }
-
