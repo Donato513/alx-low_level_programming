10ff3520fb146ec3100f64c373f2d6fabd954bf3
Donato513 <d.e.achuque@gmail.com>
2023-11-07 00:00:00 -0800

    02-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point; prints message and returns 0 for success.
+ *
+ * Return: 0 for success, non-zero for an error.
+ */
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
