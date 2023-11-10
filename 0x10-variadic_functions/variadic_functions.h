8998944f4fe55233b496a9674e543bfb67a4788e
Donato513 <d.e.achuque@gmail.com>
2023-11-09 16:32:40 -0800

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void print_char(va_list arguments);
+void print_int(va_list arguments);
+void print_float(va_list arguments);
+void print_string(va_list arguments);
 
-#endif
+
+#endif /* _VARIADIC_FUNCTIONS */
